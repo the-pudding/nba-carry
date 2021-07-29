b51c0e213bb6e8edb564a802230d56e3b9f24b88
russellgoldenberg <russellgoldenberg@gmail.com>
2021-07-29 15:57:01 -0400

    path attempt

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,8 @@ import dsv from "@rollup/plugin-dsv";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+const dev = process.env.NODE_ENV === "development";
+
 const preprocess = sveltePreprocess({
 	postcss: {
 		plugins: [autoprefixer]
@@ -32,7 +34,10 @@ const config = {
 				dsv(),
 				svg()
 			]
-		}
+		},
+		paths: {
+			base: dev ? "" : "/nba-carry"
+		},
 	}
 };
 
