48ca73827a71dc5fb2bc0dcea5c91136bc540d71
russellgoldenberg <russellgoldenberg@gmail.com>
2021-07-29 16:08:43 -0400

    update github pages

---
+++ src/components/Intro.svelte
@@ -0,0 +1,17 @@
+<script>
+  import copy from "$data/doc.json";
+</script>
+
+<section id="intro">
+  <h1 class="hed">{copy.hed}</h1>
+  <p class="dek">{copy.dek}</p>
+  <p class="byline">
+    By <a href="https://pudding.cool/author/russell-goldenberg">Russell Goldenberg</a>
+  </p>
+
+  <div class="prose">
+    {#each copy.intro as { value }}
+      <p>{@html value}</p>
+    {/each}
+  </div>
+</section>
--- src/components/Story.svelte
+++ src/components/Story.svelte
@@ -1,20 +1,9 @@
 <script>
   import copy from "$data/doc.json";
+  import Intro from "$components/Intro.svelte";
 </script>
 
-<section id="intro">
-  <h1 class="hed">{copy.hed}</h1>
-  <p class="dek">{copy.dek}</p>
-  <p class="byline">
-    By <a href="https://pudding.cool/author/russell-goldenberg">Russell Goldenberg</a>
-  </p>
-
-  <div class="prose">
-    {#each copy.intro as { value }}
-      <p>{@html value}</p>
-    {/each}
-  </div>
-</section>
+<Intro />
 
 <section id="list">
   {#each copy.list as { id, text }, i}
